38d336af4709818397e2f569afd2ed720bc1b822
allexvip <alexey_shes@mail.ru>
2022-01-22 18:13:11 +0300

    fix

--- keyboards/inline.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Bot,types
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
-import os
-
-from aiogram.types import InlineKeyboardMarkup,InlineKeyboardButton
-
-
