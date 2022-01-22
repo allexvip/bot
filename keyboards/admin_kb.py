a414540652236e782a601c3e3e31fca3f6145bda
allexvip <alexey_shes@mail.ru>
2022-01-22 18:37:59 +0300

    fix

--- keyboards/admin_kb.py
+++ keyboards/admin_kb.py
@@ -1,22 +1,18 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-
-
-
 # Кнопки клавиатуры админа
-button_load = KeyboardButton('/Загрузить')
-button_delete = KeyboardButton('/Удалить')
+button_case_admin = ReplyKeyboardMarkup(resize_keyboard=True).add(
+    KeyboardButton('/Меню')).add(
+    KeyboardButton('/Загрузить')).add(
+    KeyboardButton('/Удалить'))
 
-button_case_admin = ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton('/Меню')).add(button_load).add(button_delete)
-
-
-#кнопки сслыки
+# кнопки сслыки
 urlkb = InlineKeyboardMarkup(row_width=2)
 inline_buttons_list = [
-    InlineKeyboardButton(text='40R',url='https://40r.ru'),
-    InlineKeyboardButton(text='24ON',url='https://24on.ru'),
+    InlineKeyboardButton(text='40R', url='https://40r.ru'),
+    InlineKeyboardButton(text='24ON', url='https://24on.ru'),
 ]
 urlkb.add(*inline_buttons_list)
 
-inkb = InlineKeyboardMarkup(row_width=1).add(InlineKeyboardButton(text='Нажми меня',callback_data='www'))
+inkb = InlineKeyboardMarkup(row_width=1).add(InlineKeyboardButton(text='Нажми меня', callback_data='www'))
